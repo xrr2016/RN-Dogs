07f84e56f41dbd2f3b8a4da637d205868a70bf1d
xrr2016 <xiaoranran1993@163.com>
2018-03-09 01:42:42 +0800

    :zap: add mobx and mobx-react

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Dogs</Text>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
+
+@observer
+class App extends React.Component {
+
+  @observable secondsPassed = 0
+
+  componentDidMount = () => {
+    setInterval(() => {
+      this.secondsPassed++
+    }, 1000)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Dogssssss</Text>
+        <Text>{this.secondsPassed}</Text>
+      </View>
+    )
+  }
+}
+
+export default App
